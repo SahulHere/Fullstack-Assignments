c00482e252bcd1e2b1e8479febc87f6b9fd9bd48
SahulHere <121428188+SahulHere@users.noreply.github.com>
2024-05-24 21:11:22 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,8 +5,8 @@
 */
 
 function isAnagram(str1, str2) {
-  let a = str1.toLowerCase().split("").sort().join();
-  let b = str2.toLowerCase().split("").sort().join();
+  let a = str1.toLowerCase().split("").sort().join("");
+  let b = str2.toLowerCase().split("").sort().join("");
 
   if(a==b){
     return true;
