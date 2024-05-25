13c276ff7084679256e7dc601fc49049700e76ab
SahulHere <121428188+SahulHere@users.noreply.github.com>
2024-05-25 23:04:06 +0530

    Update todo-list.js

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -11,7 +11,49 @@
 */
 
 class Todo {
+    constructor() {
+        this.todos = [];
+    }
 
+    add(todo) {
+        this.todos.push(todo);
+    }
+
+    remove(indexOfTodo) {
+        if (indexOfTodo >= 0 && indexOfTodo < this.todos.length) {
+            this.todos.splice(indexOfTodo, 1);
+        } else {
+            throw new Error('Invalid index. Todo does not exist at the given index.');
+        }
+    }
+
+    update(index, updatedTodo) {
+        if (index >= 0 && index < this.todos.length) {
+            this.todos[index] = updatedTodo;
+        } else {
+            throw new Error('Invalid index. Todo does not exist at the given index.');
+        }
+    }
+
+    getAll() {
+        return this.todos;
+    }
+
+    get(indexOfTodo) {
+        if (indexOfTodo >= 0 && indexOfTodo < this.todos.length) {
+            return this.todos[indexOfTodo];
+        } else {
+            throw new Error('Invalid index. Todo does not exist at the given index.');
+        }
+    }
+
+    clear() {
+        this.todos = [];
+    }
 }
+
+
+
+
 
 module.exports = Todo;
