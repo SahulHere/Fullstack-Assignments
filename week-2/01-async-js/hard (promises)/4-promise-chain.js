26b5aed961a0ef5e81854219b03c88025ccb6a86
SahulHere <121428188+SahulHere@users.noreply.github.com>
2024-05-27 21:56:18 +0530

    Update 4-promise-chain.js

--- week-2/01-async-js/hard (promises)/4-promise-chain.js
+++ week-2/01-async-js/hard (promises)/4-promise-chain.js
@@ -5,20 +5,45 @@
  * Compare it with the results from 3-promise-all.js
  */
 
-function wait1(t) {
-
+function delay(time){
+   return new Promise((resolve)=>{
+    setTimeout(()=>{
+      resolve();
+    }, time);
+  });
 }
 
-function wait2(t) {
-
+function wait1(t1) {
+ 
+ return delay(t1);
 }
 
-function wait3(t) {
+function wait2(t2) {
+ 
+ return delay(t2);
+}
 
+function wait3(t3) {
+ 
+ return delay(t3);
 }
 
 function calculateTime(t1, t2, t3) {
+  startTime= Date.now();
+
+  return wait1().then(()=>{
+    wait2()
+  }).then(()=>{
+    wait3()
+  }).then(()=>{
+    const endTime = Date.now();
+    const timeTaken = endTime - startTime;
+    return timeTaken;
+  })
 
 }
+calculateTime(1000, 2000, 5000).then((timeTaken)=>{
+    console.log(`Total time taken: ${timeTaken} milliseconds`)
+  });
 
 module.exports = calculateTime;
